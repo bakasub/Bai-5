32f277c18adbe3c920d12597c80dcad41b1b7d9d
bakasub <flannoblesse@gmail.com>
2022-08-04 08:10:23 +0700

    bai tap

---
+++ ButtonScript.js
@@ -0,0 +1,16 @@
+function mouseover1(){
+    document.getElementById("submitbutton").style.color = "red";
+
+}
+function mouseout1(){
+    document.getElementById("submitbutton").style.color="black";
+}
+function mouseover2(){
+    document.getElementById("resetbutton").style.color ="red";
+}
+function mouseout2(){
+    document.getElementById("resetbutton").style.color="black";
+}
+function showmessage(){
+    alert("Cảm ơn!")
+}
